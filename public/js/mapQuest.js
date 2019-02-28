fe9e90af9c42d6cddd131fa94f726c2d49dc8740
Xuyuan Duan <x4duan@ucsd.edu>
2019-02-28 10:42:49 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key ='8DrlB9a514FxvX3xgNAwAHvxeV2DSyMx';
+	var map=L.mapquest.map('map',{
+		center: [32.878966, -117.235927],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomcontrol: false
+	});
+	L.marker([32.88, -117.236]).addTo(map);
 }
